dad1030a4ddbc12b1d2660e6e5db729fe0c12176
wecanooo <wecanooo@clebee.net>
2016-02-16 00:15:22 +0900

    unicorn.rb 추가

---
+++ config/unicorn.rb
@@ -0,0 +1,50 @@
+# Set your full path to application.
+app_dir = File.expand_path('../../', __FILE__)
+shared_dir = File.expand_path('../../../shared/', __FILE__)
+
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+# Set unicorn options
+worker_processes 8
+preload_app true
+timeout 30
+
+# Fill path to your app
+working_directory app_dir
+
+# Set up socket location
+if rails_env == 'production'
+  listen "/tmp/unicorn.nanugo.sock", :backlog => 64
+elsif rails_env == 'staging'
+  listen "/tmp/unicorn.nanugo_staging.sock", :backlog => 64
+else
+  listen 8080
+end
+
+# Loging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+
+# Set master PID location
+pid "#{shared_dir}/pids/unicorn.pid"
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
+
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_dir}/Gemfile"
+end
